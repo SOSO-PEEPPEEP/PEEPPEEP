e36fe5139851dead880f0fae06f41b75a5a9fbae
song <gomsen5@gmail.com>
2025-02-17 10:13:22 +0900

    🎉 [BE] Tada: 프로젝트의 시작!

---
+++ src/main/java/com/peeppeep/PeeppeepApplication.java
@@ -0,0 +1,13 @@
+package com.peeppeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeeppeepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeeppeepApplication.class, args);
+    }
+
+}
