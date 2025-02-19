9f95560df8e20d685ef32e89b295a228b07ca2dd
suyeon-hyeon <shonee9911@gmail.com>
2025-02-19 12:56:46 +0900

    ✨ [BE] Feat: 챌린지 Entity 생성 - DB 연결 확인용 - ENUM IsPublic 추가

--- BE/src/main/java/com/peeppeep/domain/challenge/main/entity/Challenge.java
+++ BE/src/main/java/com/peeppeep/domain/challenge/main/entity/Challenge.java
@@ -1,4 +1,44 @@
 package com.peeppeep.domain.challenge.main.entity;
 
-public class Challenge {
+import com.peeppeep.global.entity.BaseBy;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Challenge extends BaseBy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer ChallengeId;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "period")
+    private Integer period;
+
+    @Column(name = "start_at")
+    private LocalDate startAt;
+
+    @Column(name = "end_at")
+    private LocalDate endAt;
+
+    @Column(name = "is_public")
+    @Enumerated(EnumType.STRING)
+    private IsPublic isPublic;
+
+    @Column(name = "allow_join")
+    private Boolean allowJoin;
+
+    @Column(name = "streak_count")
+    private Integer streakCount;
 }
