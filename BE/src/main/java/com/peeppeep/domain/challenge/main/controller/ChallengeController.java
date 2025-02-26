f3a0175bb8a905ebf6a192c55f33928fbdd37e83
suyeon-hyeon <shonee9911@gmail.com>
2025-02-26 12:30:28 +0900

    ⚙️ [BE] Chore: import 정리

--- BE/src/main/java/com/peeppeep/domain/challenge/main/controller/ChallengeController.java
+++ BE/src/main/java/com/peeppeep/domain/challenge/main/controller/ChallengeController.java
@@ -1,6 +1,5 @@
 package com.peeppeep.domain.challenge.main.controller;
 
-import com.peeppeep.domain.challenge.main.dto.ChallengeDTO;
 import com.peeppeep.domain.challenge.main.dto.request.ChallengeRequestDTO;
 import com.peeppeep.domain.challenge.main.dto.response.ChallengeListResponseDTO;
 import com.peeppeep.domain.challenge.main.service.ChallengeService;
