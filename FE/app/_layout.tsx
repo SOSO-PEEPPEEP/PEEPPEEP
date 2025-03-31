dd4a560a07f5492b61448f1eea532637a11dcc8a
suyeon-hyeon <shonee9911@gmail.com>
2025-04-01 02:46:37 +0900

    🐛 [FE] Fix: 안드로이드 뒤로가기 버튼 수정

--- FE/app/_layout.tsx
+++ FE/app/_layout.tsx
@@ -1,12 +1,34 @@
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { View, AppState, Platform, BackHandler } from 'react-native';
 import { useFonts } from 'expo-font';
 import { TabBarProvider } from '@/context/TabBarContext';
 import Header from '@/components/ui/Header';
 import TabBar from '@/components/ui/TabBar';
 import { COLORS } from '@/constants/COLORS';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (Platform.OS !== "web") {
+      // 앱 종료 기록
+      const appStateSub = AppState.addEventListener("change", (state) => {
+        if (state === "background") {
+          AsyncStorage.setItem("appClosed", "true");
+        }
+      });
+
+      // 안드로이드 뒤로가기 막기
+      const backHandlerSub = BackHandler.addEventListener("hardwareBackPress", () => {
+        return true;
+      });
+
+      return () => {
+        appStateSub.remove();
+        backHandlerSub.remove();
+      };
+    }
+  }, []);
 
   const [fontsLoaded] = useFonts({
     'PF-Stardust': require('@/assets/fonts/PFstardust3.0.ttf'),
