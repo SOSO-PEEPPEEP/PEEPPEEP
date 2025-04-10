d566ca83a80ab514061b63e479d684b321700947
suyeon-hyeon <shonee9911@gmail.com>
2025-04-10 18:22:39 +0900

    🐛 [BE] Fix: 챌린지 수정 API 수정 - 인원이 나갈 경우, 해당 인원 ChallengeUser, Calendar, Daily 삭제 - FE detail.tsx const 위치 수정

--- BE/src/main/java/com/peeppeep/domain/challenge/main/repository/CalendarRepository.java
+++ BE/src/main/java/com/peeppeep/domain/challenge/main/repository/CalendarRepository.java
@@ -1,8 +1,12 @@
 package com.peeppeep.domain.challenge.main.repository;
 
 import com.peeppeep.domain.challenge.main.entity.Calendar;
+import com.peeppeep.domain.challenge.main.entity.Challenge;
 import com.peeppeep.domain.challenge.main.entity.ChallengeUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +14,8 @@ import java.util.Optional;
 @Repository
 public interface CalendarRepository extends JpaRepository<Calendar, Integer> {
     Optional<Calendar> findByChallengeUserAndDeletedAtIsNull(ChallengeUser challengeUser);
+
+    @Modifying
+    @Query("UPDATE Calendar c SET c.deletedAt = CURRENT_TIMESTAMP WHERE c.calendarId = :calendarId")
+    void softDelete(@Param("calendarId") Integer calendarId);
 }
--- FE/app/main/challenge/detail.tsx
+++ FE/app/main/challenge/detail.tsx
@@ -47,6 +47,7 @@ export default () => {
     const [showResultModal, setShowResultModal] = useState(false);
     const [detail, setDetail] = useState<ChallengeDetailProps | null>(null);
     const [isBookmark, setIsBookmark] = useState(false);
+    const [playEffect, setPlayEffect] = useState(false);
 
     useEffect(() => {
         const fetchChallengeDetail = async () => {
