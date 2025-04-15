09e4d9eb8bab0da0b395b9951217f0a31de21683
suyeon-hyeon <shonee9911@gmail.com>
2025-04-15 11:37:19 +0900

    🔧 [BE] Build: CORSConfig 관련 시크릿키 추가 - .gitignore에 application-security.yml 추가 - WebConfig 시크릿키 대체

--- BE/src/main/java/com/peeppeep/global/config/WebConfig.java
+++ BE/src/main/java/com/peeppeep/global/config/WebConfig.java
@@ -1,24 +1,25 @@
 package com.peeppeep.global.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081", "http://172.30.1.63:8081") // Expo 앱의 로컬/모바일 IP
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Value("${FRONT_WEB_BASE_URL}")
+    private String frontWebBaseUrl;
+
+    @Value("${FRONT_MOBILE_BASE_URL}")
+    private String frontMobileBaseUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontWebBaseUrl, frontMobileBaseUrl) // Expo 앱의 로컬/모바일 IP
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
