b98e2e5045604b98041a95be4e7e795b292fd33a
suyeon-hyeon <shonee9911@gmail.com>
2025-04-18 18:17:33 +0900

    ✨ [FE] Feat: 데일리 생성 API 연동 - 간단하게 연동 확인 - 추가적인 작업 필요

--- FE/app/main/challenge/daily/create.tsx
+++ FE/app/main/challenge/daily/create.tsx
@@ -1,5 +1,6 @@
-import { View, ScrollView, Image, TouchableOpacity } from 'react-native'
+import { View, ScrollView, Image, TouchableOpacity, Alert } from 'react-native'
 import Frame from '@/components/ui/Frame'
+import EffectSound from '@/components/common/effectSound';
 import GlobalText from '@/constants/GlobalText'
 import Margin from '@/components/ui/Margin'
 import { COLORS } from '@/constants/COLORS'
@@ -10,6 +11,7 @@ import { useState } from 'react'
 import SpeechBubble from '@/components/ui/SpeechBubble'
 import GlobalInput from '@/constants/GlobalInput'
 import * as ImagePicker from 'expo-image-picker'
+import { API_BASE_URL } from '@/constants/env';
 
 export default () => {
     const [imageUri, setImageUri] = useState<string | null>(null);
@@ -37,6 +39,42 @@ export default () => {
 
     const [content, setContent] = useState('');
 
+    //소리 효과
+    const [playEffect, setPlayEffect] = useState(false);
+
+    const createDaily = async () => {
+        const formData = new FormData();
+      
+        formData.append('day', '4');
+        formData.append('content', content);
+        formData.append('picture', {
+          uri: imageUri,
+          name: 'photo.jpg',
+          type: 'image/jpeg',
+        } as any);
+      
+        try {
+          const response = await fetch(`${API_BASE_URL}/api/challenges/15/daily`, {
+            method: 'POST',
+            body: formData,
+          });
+      
+          if (response.ok) {
+            setPlayEffect(true);
+            router.push('/main/challenge/detail');
+          } else {
+            const errData = await response.json();
+            Alert.alert("데일리 생성 실패", JSON.stringify(errData));
+          }
+        } catch (error) {
+          if (error instanceof Error) {
+            Alert.alert("네트워크 에러", error.message);
+          } else {
+            Alert.alert("알 수 없는 에러", "문제가 발생했습니다.");
+          }
+        }
+    };
+
     return (
         <Frame>
             <GlobalText style={{fontSize:16, color:COLORS.gray}}>DAILY CREATE</GlobalText>
@@ -44,7 +82,7 @@ export default () => {
                 <Margin height={8}/>
 
                 <View style={{alignItems:'center'}}>
-                    <Image source={require("@/assets/images/Stamp_NotAttempted_X2.png")}/>
+                    <Image source={require("@/assets/images/main/Stamp_NotAttempted_X2.png")}/>
                 </View>
 
                 <Margin height={8}/>
@@ -71,8 +109,8 @@ export default () => {
                     ):(
                         <View style={{backgroundColor:COLORS.green, alignItems:"center", justifyContent:'center', height:172, position: "absolute", top: 0, left: 0, right: 2}}>
                         <View>
-                            <Image source={require('@/assets/images/Add_Daily_Image_dark.png')} style={{position:'absolute',width:60, height:60,left:-1,top:-1}}/>
-                            <Image source={require('@/assets/images/Add_Daily_Image_white.png')} style={{width:60, height:60}}/>
+                            <Image source={require('@/assets/images/main/Add_Daily_Image_dark.png')} style={{position:'absolute',width:60, height:60,left:-1,top:-1}}/>
+                            <Image source={require('@/assets/images/main/Add_Daily_Image_white.png')} style={{width:60, height:60}}/>
                         </View>
                         <GlobalText style={{color:COLORS.white, fontSize:12}}>사진을 첨부해서 기록을 남기세요!</GlobalText>
                         </View>
@@ -107,7 +145,7 @@ export default () => {
             
             {/* 데일리 챌린지 생성 버튼 */}
             <TouchableOpacity
-                onPress={() => router.push('/main/challenge/detail')}
+                onPress={() => createDaily()}
                 activeOpacity={0.8}
                 style={{ alignSelf: "center" }}
             >
@@ -115,6 +153,7 @@ export default () => {
             </TouchableOpacity>
 
             <Margin height={36}/>
+        {playEffect && ( <EffectSound onPlaybackEnd={() => setPlayEffect(false)} />)}
         </Frame>
     )
 }
