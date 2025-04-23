b8a771a5b8ab7520b4e9062be1de36839ecebb01
suyeon-hyeon <shonee9911@gmail.com>
2025-04-24 01:37:40 +0900

    🐛 [BE] Fix: 챌린지 조회 권한 수정 - FRIEND_ONLY 반영 로직 작성

--- BE/src/main/java/com/peeppeep/domain/user/friend/repository/FriendRepository.java
+++ BE/src/main/java/com/peeppeep/domain/user/friend/repository/FriendRepository.java
@@ -17,4 +17,17 @@ public interface FriendRepository extends JpaRepository<Friend, String> {
 
     @Query("SELECT uf FROM Friend uf WHERE uf.receiverId = :userId AND uf.status = :status")
     Optional<Friend> requestFriendship(@Param("userId") int userId, @Param("status") String status);
+
+    @Query("""
+      SELECT CASE WHEN COUNT(f) > 0 THEN true ELSE false END
+      FROM Friend f
+      WHERE (
+        (f.senderId   = :u1 AND f.receiverId = :u2)
+        OR
+        (f.senderId   = :u2 AND f.receiverId = :u1)
+      )
+      AND f.status     = 'ACCEPTED'
+      AND f.deletedAt  IS NULL
+    """)
+    boolean existsFriendship(@Param("u1") Integer userId1, @Param("u2") Integer userId2);
 }
